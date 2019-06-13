1c2a78caa227637eb89d53b2b19187ec0370ccfe
Justin D'Souza <justinqdsouza@gmail.com>
2019-06-13 02:06:06 -0400

    added parser script

---
+++ ml/offhand_scripts/audio_dataset_parser.py
@@ -0,0 +1,26 @@
+import os
+
+dir_ = '/Users/justin/desktop/school/4th_year/FYDP/audio'
+
+if __name__ == "__main__":
+	os.chdir(dir_)
+	not_wanted = ['0', '2', '3', '4', '7', '9']
+	
+	for subdir, dirs, files in os.walk(dir_):
+	    for file in files:
+	    	print("subdir: ", subdir)
+	    	print("dirs: ", dirs)
+	    	print("file: ", file)
+	    	split_str = file.split("-", 2)
+	    	count = 0
+	    	for el in split_str:
+	    		count+=1
+
+	    		if count == 2 and str(el) in not_wanted:
+	    			print(split_str)
+	    			print(file)
+	    			print("going to remove {0} file".format(file))
+	    			up_path = subdir + '/' + file
+	    			print("path: ", up_path)
+	    			os.remove(up_path)
+	    			break
