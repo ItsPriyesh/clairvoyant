6262b3dc01039885af87b77c452a471e9547a00e
Priyesh Patel <p99patel@uwaterloo.ca>
2019-06-18 15:35:27 -0400

    update proto definition with expected data fields

--- backend/src/main/java/io/clairvoyant/ClairvoyantService.java
+++ backend/src/main/java/io/clairvoyant/ClairvoyantService.java
@@ -3,12 +3,12 @@ package io.clairvoyant;
 import io.clairvoyant.proto.Ack;
 import io.clairvoyant.proto.ClairvoyantServiceGrpc;
 import io.clairvoyant.proto.DataPoint;
+import io.clairvoyant.proto.Heartbeat;
 import io.grpc.stub.StreamObserver;
 
 final class ClairvoyantService extends ClairvoyantServiceGrpc.ClairvoyantServiceImplBase {
     @Override
     public void createDataPoint(DataPoint request, StreamObserver<Ack> responseObserver) {
-        // Do stuff with received DataPoint payload
         System.out.println(request);
 
         Ack ack = Ack.newBuilder()
@@ -17,5 +17,10 @@ final class ClairvoyantService extends ClairvoyantServiceGrpc.ClairvoyantService
 
         responseObserver.onNext(ack);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void ping(Heartbeat request, StreamObserver<Ack> responseObserver) {
+
     }
 }
