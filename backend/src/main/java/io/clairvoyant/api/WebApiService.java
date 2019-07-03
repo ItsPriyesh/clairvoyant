e0b839900d6c6b87a28b541d238b82f2101e2b06
Priyesh Patel <p99patel@uwaterloo.ca>
2019-07-03 14:00:10 -0400

    setup http get/post server on BE for FE

---
+++ backend/src/main/java/io/clairvoyant/api/WebApiService.java
@@ -0,0 +1,18 @@
+package io.clairvoyant.api;
+
+import spark.Request;
+import spark.Response;
+
+import javax.inject.Inject;
+
+public class WebApiService {
+
+    @Inject
+    public WebApiService() {
+
+    }
+
+    public String createUser(Request req, Response res) {
+        return null;
+    }
+}
