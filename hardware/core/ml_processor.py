06b6585524f2ddf90493df95325d70e2d49c4d66
Akash Kalimili <akalimil@uwaterloo.ca>
2019-07-07 14:04:22 -0400

    moved files around

---
+++ hardware/core/ml_processor.py
@@ -0,0 +1,12 @@
+#
+# Download model:
+# wget -nv --show-progress --progress=bar:force:noscroll https://max-assets-prod.s3.us-south.cloud-object-storage.appdomain.cloud/max-audio-classifier/1.0.0/assets.tar.gz --output-document=assets/assets.tar.gz
+# Extract the model tar and put the files in a folder called 'assets' in the same directory as this file.
+#
+from ml.core.model import ModelWrapper
+import os
+
+input = os.path.join("ml","samples","gunshots.wav")
+model_wrapper = ModelWrapper()
+predictions = model_wrapper._predict(input, 0)
+print(predictions)
