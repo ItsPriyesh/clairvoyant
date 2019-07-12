8e8fa0e6ca58b68b6abffb490e240564f660a08c
Priyesh Patel <p99patel@uwaterloo.ca>
2019-07-12 15:31:05 -0400

    address comments

--- backend/src/main/java/io/clairvoyant/api/DashboardApi.java
+++ backend/src/main/java/io/clairvoyant/api/DashboardApi.java
@@ -1,9 +1,7 @@
 package io.clairvoyant.api;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import io.clairvoyant.api.model.Credentials;
 import io.clairvoyant.api.model.Error;
 import io.clairvoyant.db.DataPointStore;
 import io.clairvoyant.db.UserStore;
@@ -14,38 +12,24 @@ import spark.Response;
 import javax.inject.Inject;
 
 import java.util.List;
+import java.util.Optional;
 
-import static io.clairvoyant.api.ApiService.hasParams;
+public class DashboardApi extends ApiBase {
 
-public class DashboardApi {
-
-    private final UserStore userStore;
     private final DataPointStore dataPointStore;
-    private final Gson gson;
 
     @Inject
     public DashboardApi(UserStore userStore, DataPointStore dataPointStore, Gson gson) {
-        this.userStore = userStore;
+        super(gson, userStore);
         this.dataPointStore = dataPointStore;
-        this.gson = gson;
     }
 
-
     public String getDataPoints(Request req, Response res) {
-        if (!hasParams(req, "user_id", "session_token")) {
-            res.status(400);
-            return gson.toJson(Error.create("Missing credentials!"));
-        }
+        Optional<Error> err = authenticateUser(req, res);
+        if (err.isPresent()) return toJson(err.get());
 
         int userId = Integer.parseInt(req.queryParams("user_id"));
-        boolean isValid = userStore.isUserSessionValid(
-                userId, req.queryParams("session_token")).blockingGet();
-        if (!isValid) {
-            res.status(401);
-            return gson.toJson(Error.create("Invalid credentials!"));
-        }
-
         List<DataPoint> data = dataPointStore.getDataPoints(userId).blockingGet();
-        return gson.toJson(data, new TypeToken<List<DataPoint>>(){}.getType());
+        return toJson(data, new TypeToken<List<DataPoint>>() {}.getType());
     }
 }
--- backend/src/main/java/io/clairvoyant/api/LoginApi.java
+++ backend/src/main/java/io/clairvoyant/api/LoginApi.java
@@ -12,38 +12,24 @@ import spark.Response;
 
 import javax.inject.Inject;
 import java.sql.Timestamp;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Optional;
 import java.util.UUID;
 
-import static io.clairvoyant.api.ApiService.hasParams;
-
-public class LoginApi {
+public class LoginApi extends ApiBase {
 
     private final UserStore userStore;
     private final PasswordManager passManager;
-    private final Gson gson;
 
     @Inject
     public LoginApi(UserStore userStore, PasswordManager passManager, Gson gson) {
+        super(gson, userStore);
         this.userStore = userStore;
         this.passManager = passManager;
-        this.gson = gson;
     }
 
     public String createUser(Request req, Response res) {
-        return gson.toJson(createUserImpl(req, res));
-    }
-
-    public String login(Request req, Response res) {
-        return gson.toJson(loginImpl(req, res));
-    }
-
-    private Object createUserImpl(Request req, Response res) {
-        if (!hasParams(req, "firstName", "lastName", "email", "password")) {
-            res.status(400);
-            return Error.create("Required parameters not specified!");
-        }
+        Optional<Error> err = assertParams(req, res, "firstName", "lastName", "email", "password");
+        if (err.isPresent()) return toJson(err.get());
 
         String sessionToken = UUID.randomUUID().toString();
 
@@ -56,36 +42,33 @@ public class LoginApi {
                 .setSessionToken(sessionToken)
                 .build();
 
-        System.out.println(user);
-        Throwable error = userStore.insert(user).blockingGet();
-        if (error == null) {
+        Throwable insertErr = userStore.insert(user).blockingGet();
+        if (insertErr == null) {
             res.status(200);
-            return SessionToken.create(sessionToken);
+            return toJson(SessionToken.create(sessionToken));
         } else {
             res.status(500);
-            return Error.create(error.getMessage());
+            return toJson(Error.create(insertErr.getMessage()));
         }
     }
 
-    private Object loginImpl(Request req, Response res) {
-        if (!hasParams(req, "email", "password")) {
-            res.status(400);
-            return Error.create("Required parameters not specified!");
-        }
+    public String login(Request req, Response res) {
+        Optional<Error> error = assertParams(req, res, "email", "password");
+        if (error.isPresent()) return toJson(error);
 
         Maybe<User> userMaybe = userStore.getUser(req.queryParams("email"));
         if (userMaybe.isEmpty().blockingGet()) {
             res.status(400);
-            return Error.create("Invalid email!");
+            return toJson(Error.create("Invalid email!"));
         }
 
         User user = userMaybe.blockingGet();
         if (passManager.check(req.queryParams("password"), user.passwordHash())) {
             res.status(200);
-            return SessionToken.create(user.sessionToken());
+            return toJson(SessionToken.create(user.sessionToken()));
         } else {
             res.status(400);
-            return Error.create("Invalid password!");
+            return toJson(Error.create("Invalid password!"));
         }
     }
 }
