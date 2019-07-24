0324b68412261a42cde1507b843f72f256f1016e
Sathoshi Kumarawadu <sathoshi.k@gmail.com>
2019-07-24 01:19:30 -0400

    fix ml model

--- node/core/ml_processor.py
+++ node/core/ml_processor.py
@@ -11,10 +11,11 @@ import multiprocessing
 import time
 import warnings
 import tensorflow as tf
-import datetime
+import time
 import pandas as pd
 import numpy as np
 import librosa
+import traceback
 
 from ml.core.model import ModelWrapper
 from sklearn.preprocessing import LabelEncoder
@@ -22,13 +23,11 @@ from sklearn.preprocessing import LabelEncoder
 # import tensorflow.keras.utils.load as load
 from keras.utils import to_categorical
 from keras.models import load_model
+from clairvoyant_data import PacketBuilder
+from clairvoyant_data import PacketBuilder
 
 MODEL_PATH = os.path.join('ml','assets','weights.best.resampled_cnn6.hdf5')
 CSV_PATH = os.path.join('ml','assets','final_labeled_df.csv')
-
-model_wrapper = ModelWrapper()
-IBM_DF = model_wrapper.indices
-LABEL_MAPPING = IBM_DF[['display_name', 'class']].set_index('display_name').T.to_dict('dict')
 
 ## fixed params
 NUM_ROWS = 40
@@ -39,7 +38,7 @@ JUSTIN_DF = pd.read_csv(CSV_PATH)
 y = np.array(JUSTIN_DF.class_label.tolist())
 le = LabelEncoder()
 yy = to_categorical(le.fit_transform(y)) 
-JUSTIN_MODEL = load_model(MODEL_PATH)
+# JUSTIN_MODEL = load_model(MODEL_PATH)
 
 def init(ml_q,ml_init_q):
 
@@ -49,7 +48,8 @@ def init(ml_q,ml_init_q):
     warnings.filterwarnings("ignore")
     tf.logging.set_verbosity(tf.logging.ERROR)
 
-    print("ML: Entering superloop")
+    model_wrapper = ModelWrapper()
+
     while (1):
         
         #check for new files
@@ -66,7 +66,7 @@ def init(ml_q,ml_init_q):
                 ml_init_q.put(justin_packet)
 
             ## IBM MODEL
-            ibm_packet = ibm_model(file_path)
+            ibm_packet = ibm_model(file_path, model_wrapper)
             if ibm_packet != "NOISE":
                 ml_init_q.put(ibm_packet)
 
@@ -74,7 +74,9 @@ def init(ml_q,ml_init_q):
 
 def justin_model(file_path):
     prediction_feature = extract_features(file_path)
+
     prediction_feature = prediction_feature.reshape(1, NUM_ROWS, NUM_COLUMNS, NUM_CHANNELS)
+    JUSTIN_MODEL = load_model(MODEL_PATH)
     predicted_vector = JUSTIN_MODEL.predict_classes(prediction_feature)
     predicted_class = le.inverse_transform(predicted_vector)
     keys = le.classes_
@@ -89,41 +91,55 @@ def justin_model(file_path):
         count+=1
 
     predictions = sorted(classes_dict.items(), key=lambda x: x[1], reverse=True)
-    prediction = "J " + predictions[0][0]
+    ans = predictions[0][0]
     confidence = predictions[0][1]
     built_packet = ''
 
-    # print("Justin model prediction: ", prediction)
-    # print("Justin model confidence: ", confidence)
+    print("\r\nJustin prediction: {}, Justin confidence: {}\r\n".format(ans, confidence))
     
-    if str(prediction) == 'noise':
+    if str(ans) == 'noise':
         built_packet = "NOISE"
     
     else:
-        timestamp = datetime.datetime.now().time()
-        ml_payload = clairvoyant_data.MlPayload(_battery_lvl = 100, _timestamp = timestamp, _classification = prediction, _confidence = confidence)
-        ml_packet = clairvoyant_data.PacketBuilder(_type = "ML_CLASS",_node_id = clairvoyant.CURRENT_NODE, _message_id = None, _payload = ml_payload, _ttl = None)
+        timestamp = round(time.time())
+        prediction = "J " + ans
+        # ml_payload = clairvoyant_data.MlPayload(_battery_lvl = 100.0, _timestamp = timestamp, _classification = prediction, _confidence = confidence)
+        ml_payload = clairvoyant_data.MlPayload()
+        ml_payload._battery_lvl = 100.0
+        ml_payload._timestamp = timestamp
+        ml_payload._classification = prediction
+        ml_payload._confidence = confidence
+        ml_packet = PacketBuilder().set_type("ML_CLASS").set_node_id(clairvoyant.CURRENT_NODE).set_message_id().set_payload(ml_payload).set_ttl().set_retry_count().set_hop_count()
         built_packet = ml_packet.build()
     
     return built_packet
 
-def ibm_model(file_path):
+def ibm_model(file_path, model_wrapper):
+    # model_wrapper = ModelWrapper()
+    IBM_DF = model_wrapper.indices
+    LABEL_MAPPING = IBM_DF[['display_name', 'class']].set_index('display_name').T.to_dict('dict')
     ibm_predictions = model_wrapper._predict(file_path, 0)
-
-    # print("IBM model predictions: ", ibm_predictions)
 
     sub_label = ibm_predictions[0][1]
     prediction = "I " + LABEL_MAPPING.get(sub_label)['class']
     confidence = ibm_predictions[0][2]
+
+    print("\r\nIBM prediction: {}, IBM confidence: {}\r\n".format(prediction, confidence))
+
     built_packet = ''
         
     if str(prediction) == 'noise':
         built_packet = "NOISE"
 
     else:
-        timestamp = datetime.datetime.now().time()
-        ml_payload = clairvoyant_data.MlPayload(_battery_lvl = 100, _timestamp = timestamp, _classification = prediction, _confidence = confidence)
-        ml_packet = clairvoyant_data.PacketBuilder(_type = "ML_CLASS",_node_id = clairvoyant.CURRENT_NODE, _message_id = None, _payload = ml_payload, _ttl = None)
+        timestamp = round(time.time())
+        # ml_payload = clairvoyant_data.MlPayload(_battery_lvl = 100.0, _timestamp = timestamp, _classification = prediction, _confidence = confidence)
+        ml_payload = clairvoyant_data.MlPayload()
+        ml_payload._battery_lvl = 100.0
+        ml_payload._timestamp = timestamp
+        ml_payload._classification = prediction
+        ml_payload._confidence = confidence
+        ml_packet = PacketBuilder().set_type("ML_CLASS").set_node_id(clairvoyant.CURRENT_NODE).set_message_id().set_payload(ml_payload).set_ttl().set_retry_count().set_hop_count()
         built_packet = ml_packet.build()
 
     return built_packet
@@ -145,6 +161,7 @@ def extract_features(file_name):
             return None
 
     except Exception as e:
+        traceback.print_exc()
         print("ERROR: Cannot parse the following file: ", file_name)
         return None
 
