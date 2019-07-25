8c8b98e2f984cc2301677d9f8b02f22d414193a4
Priyesh Patel <p99patel@uwaterloo.ca>
2019-07-24 21:23:55 -0400

    allow cross origin resource sharing

--- backend/src/main/java/io/clairvoyant/ClairvoyantServer.java
+++ backend/src/main/java/io/clairvoyant/ClairvoyantServer.java
@@ -51,11 +51,14 @@ public class ClairvoyantServer {
 
         Spark.webSocket("/listenDataPoint", component.createSocketHandler());
 
-        Spark.before((req, res) -> {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Content-Type", "application/json");
-            res.header("X-Content-Type-Options", "nosniff");
-        });
+        enableCORS("*", "POST, GET", "*");
+
+
+//        Spark.before((req, res) -> {
+//            res.header("Access-Control-Allow-Origin", "*");
+//            res.header("Content-Type", "application/json");
+//            res.header("X-Content-Type-Options", "nosniff");
+//        });
 
         LoginApi login = component.createLoginApi();
         Spark.post("/createUser", login::createUser);
@@ -64,5 +67,31 @@ public class ClairvoyantServer {
         DashboardApi dash = component.createDashboardApi();
         Spark.get("/datapoints", dash::getDataPoints);
         Spark.get("/nodes", dash::getNodes);
+    }
+
+    private static void enableCORS(final String origin, final String methods, final String headers) {
+
+        Spark.options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        Spark.before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
+            // Note: this may or may not be necessary in your particular application
+            response.type("application/json");
+        });
     }
 }
