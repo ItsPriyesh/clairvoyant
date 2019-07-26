6c03ebc1b121ee29a2830487337b981389e4dd64
Priyesh Patel <p99patel@uwaterloo.ca>
2019-07-25 22:19:35 -0400

    update node config

--- node/core/clairvoyant.py
+++ node/core/clairvoyant.py
@@ -1,7 +1,8 @@
 #TODO(Sathoshi): Device provisioning to give identification information to each node
-CURRENT_NODE = "curr"
+CURRENT_NODE = "1"
 LORA_NODE_ID = 1
 LORA_NETWORK_ID = 1
-RPC_PORT = '8081'
-RPC_ADDRESS = 'localhost'
-FORCE_GATEWAY = False
+RPC_PORT = '8080'
+RPC_ADDRESS = '40.114.122.121'
+FORCE_GATEWAY = True
+
