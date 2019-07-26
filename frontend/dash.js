cff7c9bdb46efaa4c8468a10ca2b2eb8dc1de15f
Brendan D'Souza <b4dsouza@uwaterloo.ca>
2019-07-25 23:27:57 -0400

    remove dp array in fe, update chart

--- frontend/dash.js
+++ frontend/dash.js
@@ -10,7 +10,7 @@ let credentials = {
 };
 
 let historyTable = $("#history_table").find('tbody');
-var dps = [];
+var pie;
 
 var API_BASE;
 var SOCKET_BASE;
@@ -20,14 +20,13 @@ $(document).ready(function() {
     API_BASE = config.api_url;
     SOCKET_BASE = config.socket_url;
   httpGET('/nodes', (nodes) => {
-    console.log('Received nodes ' + JSON.stringify(nodes));
+    // console.log('Received nodes ' + JSON.stringify(nodes));
     bindNodes(nodes);
     bindNodeSummary(nodes);
   });
 
   httpGET('/datapoints', (datapoints) => {
-    console.log('Received datapoints ' + JSON.stringify(datapoints));
-    dps = datapoints;
+    // console.log('Received datapoints ' + JSON.stringify(datapoints));
     bindEventBreakdown(datapoints);
     bindHistory(datapoints);
   });
@@ -38,11 +37,10 @@ $(document).ready(function() {
   }
   webSocket.onmessage = function (msg) {
     var dp = JSON.parse(msg.data);
-    dps.push(dp);
-    console.log(dps);
+    console.log(dp);
     appendHistory(dp);
     animateDataPointReceived(dp);
-    bindEventBreakdown(dps);
+    updateChart(pie, dp.classification, dp);
   };
   });
 });
@@ -100,11 +98,9 @@ bindNodes = function(nodes) {
 
 bindNodeSummary = function(nodes) {
   let nodesTable = $("#nodes_table").find('tbody');
-  let historyTable = $("#history_table").find('tbody');
   for(var i = 0; i < nodes.length; i++) {
     let n = nodes[i];
     let row = `<tr><td>${n.id}</td><td>${n.battery_level}%</td></tr>`;
-    console.log(n.battery_level);
     nodesTable.append(row);
   }
 }
@@ -122,9 +118,7 @@ appendHistory = function(d) {
 }
 
 bindEventBreakdown = function(datapoints) {
-  console.log("bindEventBreakdown " +datapoints );
   let eventTypes = countByType(datapoints);
-  console.log(eventTypes);
   var config = {
     type: 'pie',
     data: {
@@ -146,7 +140,17 @@ bindEventBreakdown = function(datapoints) {
   var ctx = document.getElementById('event-pie').getContext('2d');
   ctx.width = 1;
   ctx.height = 1;
-  let pie = new Chart(ctx, config);
+  pie = new Chart(ctx, config);
+}
+
+updateChart = function(chart, label, data) {
+  for(var i = 0; i < chart.data.datasets[0].data.length; i++) {
+    if(label === chart.data.labels[i]) {
+      chart.data.datasets[0].data[i]++;
+    }
+  }
+  
+  chart.update();
 }
 
 countByType = function(datapoints) {
