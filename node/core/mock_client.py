5d99430be2f64c0016a110673fcae5a5d1d76643
Priyesh Patel <p99patel@uwaterloo.ca>
2020-02-23 16:52:13 -0500

    better pie chart

--- node/core/mock_client.py
+++ node/core/mock_client.py
@@ -7,7 +7,7 @@ channel = grpc.insecure_channel("localhost:8080")
 stub = grpc_service.ClairvoyantServiceStub(channel)
 
 datapoint = grpc_model.DataPoint(message_id="sss"+str(time()), node_id="2", 
-	timestamp=round(time()), classification="VEHICLE", confidence=0.42)
+	timestamp=round(time()), classification="BOMB", confidence=0.42)
 ack = stub.CreateDataPoint(datapoint)
 print(ack)
 
