8dffe279414c262a58ffbbe06468af39f8075f9f
Sathoshi Kumarawadu <sathoshi.k@gmail.com>
2020-03-08 19:38:17 -0400

    update ml_cloud_procc to point to the azure API

--- node/core/ml_cloud_processor.py
+++ node/core/ml_cloud_processor.py
@@ -9,7 +9,7 @@ import json
 from clairvoyant_data import PacketBuilder
 from clairvoyant_data import PacketBuilder
 
-_API_ENDPOINT = "http://localhost:5000/model/predict"
+_API_ENDPOINT = "http://168.62.177.105:5000/model/predict"
 
 def init(input_q, output_q):
     print("Initializing Machine Learning Process...")
