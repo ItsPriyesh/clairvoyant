7d3a4bc18bfae2937d4d98b102581c3b5a0b6503
Akash Kalimili <akalimil@uwaterloo.ca>
2020-03-10 22:21:11 -0400

    hola

--- node/core/data_processor.py
+++ node/core/data_processor.py
@@ -6,9 +6,10 @@ import contextlib
 
 def init(audio_q, ml_q):
     print("Initializing Data Processing Process...")
-    FRAME_LENGTH = 44100*10 #10 sec
+    FRAME_LENGTH = 44100*4 #10 sec
 
     shift = int(176400) #4 second
+    shift = FRAME_LENGTH
     files = []
     frames_1 = []
     frames_2 = []
