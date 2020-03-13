3c12b80c6faaa9ad7a4e98207b989432c4efe291
Brendan D'Souza <b4dsouza@uwaterloo.ca>
2020-03-12 22:44:08 -0400

    update sql script

--- backend/sql/createTestData.sql
+++ backend/sql/createTestData.sql
@@ -9,17 +9,17 @@ VALUES ('1','2019-01-01 11:38:01.00', @testUser, 92.3),
 	   ('2','2019-01-01 12:45:01.00', @testUser, 10.2),
 	   ('3','2019-01-01 10:13:01.00', @testUser, 100);
 
-SELECT @node := node_id FROM Node
-WHERE user_id = @testUser
-LIMIT 1;
+-- SELECT @node := node_id FROM Node
+-- WHERE user_id = @testUser
+-- LIMIT 1;
 
--- create datapoints
-INSERT INTO DataPoint (data_point_id, node_id, created_at, classification, confidence)
-VALUES ('c56be2cd0f0244d7aa3a9117ca29579a', @node, '2019-01-01 10:28:01.00', 'EXPLOSION', 0.92),
-	   ('7304b98072304a91afdb7da63cda9ec3', @node, '2019-01-01 9:38:01.00', 'GUNSHOT', 0.45),
-	   ('fc704b40f0904bb7ab7fddacc0a2f860', @node, '2019-01-01 7:38:01.00', 'VEHICLE', 0.72);
+-- -- create datapoints
+-- INSERT INTO DataPoint (data_point_id, node_id, created_at, classification, confidence)
+-- VALUES ('c56be2cd0f0244d7aa3a9117ca29579a', @node, '2019-01-01 10:28:01.00', 'EXPLOSION', 0.92),
+-- 	   ('7304b98072304a91afdb7da63cda9ec3', @node, '2019-01-01 9:38:01.00', 'GUNSHOT', 0.45),
+-- 	   ('fc704b40f0904bb7ab7fddacc0a2f860', @node, '2019-01-01 7:38:01.00', 'VEHICLE', 0.72);
 
-INSERT INTO MotionEvent (motion_event_id, node_id, created_at, motion_type, orientation, roll, pitch, yaw)
-VALUES ('c56be2cd0f0244d7aa3a9117ca29579a', @node, '2019-01-01 10:28:01.00', 'fbrei', 'up', 1, 2, 3),
-	   ('7304b98072304a91afdb7da63cda9ec3', @node, '2019-01-01 9:38:01.00', 'fewfw','down', 4, 5, 6),
-	   ('fc704b40f0904bb7ab7fddacc0a2f860', @node, '2019-01-01 7:38:01.00', 'VEHdnjndICLE','left', 180, 181, 182);
+-- INSERT INTO MotionEvent (motion_event_id, node_id, created_at, motion_type, orientation, roll, pitch, yaw)
+-- VALUES ('c56be2cd0f0244d7aa3a9117ca29579a', @node, '2019-01-01 10:28:01.00', 'fbrei', 'up', 1, 2, 3),
+-- 	   ('7304b98072304a91afdb7da63cda9ec3', @node, '2019-01-01 9:38:01.00', 'fewfw','down', 4, 5, 6),
+-- 	   ('fc704b40f0904bb7ab7fddacc0a2f860', @node, '2019-01-01 7:38:01.00', 'VEHdnjndICLE','left', 180, 181, 182);
